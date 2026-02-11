0802821679f3dca35899bd2a9827a4e00aca400a
andrewmarcello1-dev <andrewmarcello1@gmail.com>
2026-02-10 20:05:59 -0500

    Update Linearization_of_Nonlinear_System_Using_Jacobian.m

--- Communication, Simulation, and Control/Linearization_of_Nonlinear_System_Using_Jacobian.m
+++ Communication, Simulation, and Control/Linearization_of_Nonlinear_System_Using_Jacobian.m
@@ -111,3 +111,39 @@ v = 2*x1g.^2 - x2g;
 quiver(x1g,x2g,u,v,0.7);
 
 axis([-3 3 -6 6]);
+
+%% Single Line
+x01 = [1 0]';
+x02 = [-1 1]';
+x03 = [0 1]';
+
+x0_mat = [x01 x02 x03];
+
+figure; hold on; grid on;
+xlabel('x_1'); ylabel('x_2');
+title('Phase-plane trajectories: nonlinear system exact solution compared with linearization');
+
+for k = 1:length(x0_mat(1,:))
+    [t,x] = ode45(f_exact, tspan, x0_mat(:,k));
+    plot(x(:,1), x(:,2), 'LineWidth', 2.0);
+    [t1,x1] = ode45(@(t,x) f(t,x,A1,xe1), tspan, x0_mat(:,k));
+    plot(x1(:,1), x1(:,2),'--', 'LineWidth', 1.0);
+    [t2,x2] = ode45(@(t,x) f(t,x,A2,xe2), tspan, x0_mat(:,k));
+    plot(x2(:,1), x2(:,2),'-*', 'LineWidth', 1.0);
+    [t3,x3] = ode45(@(t,x) f(t,x,A3,xe3), tspan, x0_mat(:,k));
+    plot(x3(:,1), x3(:,2),'-d', 'LineWidth', 1.0);
+end
+
+
+% Mark equilibrium points
+plot(0,0,'ko','MarkerFaceColor','k','MarkerSize',6);
+plot(1,2,'bo','MarkerFaceColor','b','MarkerSize',6);
+plot(-1,2,'ro','MarkerFaceColor','r','MarkerSize',6);
+
+legend('Exact: x0 = [1 0]''','Linearization About eq1: x0 = [1 0]''','Linearization About eq2: x0 = [1 0]''','Linearization About eq3: x0 = [1 0]''', ...
+    'Exact: x0 = [-1 1]''','Linearization About eq1: x0 = [-1 1]''','Linearization About eq2: x0 = [-1 1]''','Linearization About eq3: x0 = [-1 1]''', ...
+    'Exact: x0 = [0 1]''','Linearization About eq1: x0 = [0 1]''','Linearization About eq2: x0 = [0 1]''','Linearization About eq3: x0 = [0 1]''', ...
+    'x_{e1}','x_{e2}','x_{e3}','Loaction','best')
+
+xlim([-3 3])
+ylim([-2 4])
